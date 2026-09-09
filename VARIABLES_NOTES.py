c4b40854b8dac71342af87b9723aed1c398928b5
CS20260 <alexia.boone@ucas-edu.net>
2026-09-09 14:52:19 -0600

    VARIABLES_NOTES

---
+++ VARIABLES_NOTES.py
@@ -0,0 +1,7 @@
+# AB, Variables Notes, 9/9/2026 <= for people to read, not for computer
+
+name = input("Tell me your name.")
+
+print("Hockey is the best sport ever!!!!!")
+print("The ice rink is the best place to be.")
+print("Hello" ,name, "welcome to class.")
--- test.py
+++ test.py
@@ -1,3 +1,10 @@
-# AB, 7th, First program.
+# AB, 7th, *******NOTES*******
 
 print("Hello World!") 
+
+###### pseudocode notes ######
+# Break Down logic to Smaller pieces
+# Each line is own step
+# indent steps under blocks
+
+###### veriables ######
