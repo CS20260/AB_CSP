47583391b76a18b1a47323092dd9783f4dc5df0f
CS20260 <alexia.boone@ucas-edu.net>
2026-09-10 14:34:03 -0600

    Variables Practice

---
+++ Variables_Practice.py
@@ -0,0 +1,25 @@
+# AB, 10/9/26, Variables Practice
+
+# name variable
+your_name = input("What is your name? ")
+# number between 1 and 10
+one_to_ten = input("Pick a number 1-10. ")
+# number between 100 and 10,000
+one_h_ten_h = input("Pick a number 100-10,000. ")
+# what you had for breakfast
+your_breakfast = input("What did you have for breakfast? ")
+# what your favorite color is
+fav_color = input("What if you favorite color? ")
+# a school name
+school_name = input("What is a name of a school? ")
+# the year
+year = input("What year is it? ")
+# your eye color
+eye_color = input("What is your eye color? ")
+# someone's age
+age = input("How old is someone? (It can be anyone...) ")
+# your favorite subject in school
+fav_subject = input("What is your favorite subject in school? ")
+
+
+print("Your name is", your_name, ". You picked a", one_to_ten, ", and then a", one_h_ten_h, ". You had", your_breakfast, "for breakfast. Your favorite color is", fav_color, ". The school name was", school_name, ". the year is", year, ". Your eye color is", eye_color, ". The age you put in is", age, ". Your favorite subject is", fav_subject, ".")
