a84cfd988c957fddcf54c566b86ac9d3ad88218a
CS20260 <alexia.boone@ucas-edu.net>
2026-09-15 14:39:11 -0600

    revised version

--- hello_user.py
+++ hello_user.py
@@ -1,6 +1,14 @@
 # AB, 15/9/2026 Hello User Assingment.
 
 # what is your name
-name = input("What is your name???")
-# print hello name
-print("Hello", name)
+
+# print hello name while loop
+while True:
+    name = input("What is your name???").strip().title()
+    if name.isnumeric():
+        print("Dude. No numbers.")
+    elif " " in name:
+        print(f"Gonna be formal eh? My most humble greetings {name}.")
+    else:
+        break
+print(f"Hello {name}.")
